7e556e5e772eb286361072addb602769ce2adfd2
AlexSoto <alejandrosotoserafin@gmail.com>
2024-08-23 08:26:47 -0600

    conversions

---
+++ Module 2 Variables Data Types Type Casting and Comments/Section 2 Data Types/12.conversions.js
@@ -0,0 +1,11 @@
+//Conversions
+"use strict";
+
+const num1 = 42
+
+const strFromNum1 = String(num1);
+console.log(strFromNum1);
+const strFromNum2 = String(8);
+console.log(strFromNum2);
+const strFromBool = String(true);
+console.log(strFromBool)

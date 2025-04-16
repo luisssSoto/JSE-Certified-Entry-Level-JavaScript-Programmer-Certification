466e82899fe4f0f2b7b896c32bbeb7f88e9fe1ed
AlexSoto <alejandrosotoserafin@gmail.com>
2025-04-16 06:29:42 -0500

    add for loop file

---
+++ Module4ControlFlow/Section2Loops/3.forLoop.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// for loop has three main parts:
+// 1. initialization 
+// 2. condition
+// 3. increment or decrement
+// Any of these values could be empty, But affects the normal behavior of the for loop
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+};
